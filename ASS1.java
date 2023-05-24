bc452905b5108ec252fcf668efef6e29b9f0c9ba
Likinesh <129784483+Likinesh@users.noreply.github.com>
2023-05-25 00:28:11 +0530

    Update ASS1.java

--- ASS1.java
+++ ASS1.java
@@ -1,1 +1,19 @@
-
+import java.util.*;
+public class ass1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M=sc.nextInt();
+        if(M%3==0 && M%5==0){
+            System.out.print("Good Number");
+        }
+        if(M%3==0 && M%5!=0){
+            System.out.print("Bad Number");
+        }
+        if(M%3!=0 && M%5==0){
+            System.out.print("Poor Number");
+        }
+        if(M%3!=0 && M%5!=0){
+            System.out.print("-1");
+        }
+    }
+}
